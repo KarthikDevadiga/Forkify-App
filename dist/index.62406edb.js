7ad75767b58647b94f3e053d4de4dab08ced99ec
Karthik K Devadiga <kdevadiga8055@gmail.com>
2022-03-17 15:31:46 +0530

    fa 1
    recived api from server

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -5,7 +5,33 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+/*
+ * fetching api from https://forkify-api.herokuapp.com/v2 site
+ * consuming only 1 recipe now
+ */ const getData = async function() {
+    try {
+        const response = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+        const data = await response.json();
+        if (data.status !== 'success') throw new Error('Check your Data');
+        let { recipe  } = data.data;
+        recipe = {
+            cookingTime: recipe.cooking_time,
+            id: recipe.id,
+            imageUrl: recipe.image_url,
+            ingredients: recipe.ingredients,
+            publisher: recipe.publisher,
+            servings: recipe.servings,
+            sourceUrl: recipe.source_url,
+            title: recipe.title
+        };
+        console.log(recipe);
+    } catch (err) {
+        console.log(`error MANUAL ${err}`);
+    }
+}; // getData();
+ // window.location.reload();
 
 //# sourceMappingURL=index.62406edb.js.map
--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,34 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+/*
+ * fetching api from https://forkify-api.herokuapp.com/v2 site
+ * consuming only 1 recipe now
+ */
+const getData = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await response.json();
+    if (data.status !== 'success') throw new Error('Check your Data');
+    let { recipe } = data.data;
+    recipe = {
+      cookingTime: recipe.cooking_time,
+      id: recipe.id,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+    };
+    console.log(recipe);
+  } catch (err) {
+    console.log(`error MANUAL ${err}`);
+  }
+};
+
+// getData();
+
+// window.location.reload();
